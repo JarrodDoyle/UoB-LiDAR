585db3433e2aaf841176c80c5a68a40bf52e903e
TBExtent <jedpreist@gmail.com>
2019-11-26 10:38:34 +0000

    remove unneccessary test, update gitignore

--- backend/lidar/src/test/java/com/lidar/lidar/LidarApplicationTests.java
+++ backend/lidar/src/test/java/com/lidar/lidar/LidarApplicationTests.java
@@ -23,11 +23,11 @@ class LidarApplicationTests {
 	@Autowired
     private MockMvc mvc;
 
-	@Test
+	/*@Test
 	public void getLidar() throws Exception {
 		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
-	}
+	}*/
 
 }
