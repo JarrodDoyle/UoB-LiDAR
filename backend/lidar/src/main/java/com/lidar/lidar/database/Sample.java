146ad0defcdad1ab3b9419a608704f12664de4e5
TBExtent <jedpreist@gmail.com>
2019-12-11 13:43:34 +0000

    database

---
+++ backend/lidar/src/main/java/com/lidar/lidar/database/Sample.java
@@ -0,0 +1,18 @@
+package com.lidar.lidar.database;
+
+public class Sample {
+    Long id;
+
+    Float direction30m;
+    Float direction40m;
+    Float direction60m;
+    Float direction80m;
+    Float direction100m;
+
+    Float speed30m;
+    Float speed40m;
+    Float speed60m;
+    Float speed80m;
+    Float speed100m;
+
+}
