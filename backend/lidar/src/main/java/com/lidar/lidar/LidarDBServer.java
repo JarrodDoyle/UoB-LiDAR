d68611e589bd7030b1e5e139ea14ef2fe9f3c5ef
TBExtent <jedpreist@gmail.com>
2019-12-11 13:44:03 +0000

    add database stuff

---
+++ backend/lidar/src/main/java/com/lidar/lidar/LidarDBServer.java
@@ -0,0 +1,71 @@
+package com.lidar.lidar;
+
+import com.lidar.lidar.database.*;
+
+import java.util.Optional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+
+@RestController
+public class LidarDBServer {
+
+    @Autowired
+    SampleTable samples;
+
+    @Autowired
+    TestTable tests;
+
+    public static void main(String[] args) {
+        SpringApplication.run(LidarDBServer.class, args);
+    }
+
+    @RequestMapping("/test/database/create")
+    public String testCreate(@RequestParam(name="name", required=false, defaultValue="test") String name) {
+        Test test = new Test(name);
+        tests.save(test);
+        return test.toString();
+    }
+
+    @RequestMapping("/test/database/read")
+    public String testRead(@RequestParam(name="id", required=false, defaultValue = "1") Long id) {
+        Optional<Test> result = tests.findById(id);
+        if (result.isPresent()) {
+            return result.get().toString();
+        }
+        else {
+            return "Entry not found.";
+        }
+    }
+
+    @RequestMapping("/test/database/update")
+    public String testUpdate(@RequestParam(name="id", required = false, defaultValue = "1") Long id, @RequestParam(name="name", required = false, defaultValue = "test") String name) {
+        Optional<Test> result = tests.findById(id);
+        if (result.isPresent()) {
+            Test test = result.get();
+            test.setName(name);
+            return tests.save(test).toString();
+        }
+        else {
+            return "Entry not found.";
+        }
+    }
+
+    @RequestMapping("/test/database/delete")
+    public String testDelete(@RequestParam(name="id", required = false, defaultValue = "1") Long id) {
+        if (tests.existsById(id)) {
+            tests.deleteById(id);
+            return "Entry deleted.";
+        }
+        else {
+            return "Entry not found.";
+        }
+    }
+}
--- backend/lidar/src/main/java/com/lidar/lidar/database/Sample.java
+++ backend/lidar/src/main/java/com/lidar/lidar/database/Sample.java
@@ -1,18 +1,62 @@
 package com.lidar.lidar.database;
 
+import javax.persistence.*;
+
+@Entity @Table(name = "samples")
 public class Sample {
+    @Id @GeneratedValue @Column(name = "id")
     Long id;
 
-    Float direction30m;
-    Float direction40m;
-    Float direction60m;
-    Float direction80m;
-    Float direction100m;
+    @Column(name = "direction30m")
+    Double direction30m;
+    @Column(name = "direction40m")
+    Double direction40m;
+    @Column(name = "direction60m")
+    Double direction60m;
+    @Column(name = "direction80m")
+    Double direction80m;
+    @Column(name = "direction100m")
+    Double direction100m;
 
-    Float speed30m;
-    Float speed40m;
-    Float speed60m;
-    Float speed80m;
-    Float speed100m;
+    @Column(name = "speed30m")
+    Double speed30m;
+    @Column(name = "speed40m")
+    Double speed40m;
+    @Column(name = "speed60m")
+    Double speed60m;
+    @Column(name = "speed80m")
+    Double speed80m;
+    @Column(name = "speed100m")
+    Double speed100m;
 
+    @Column(name = "ti30m")
+    Double ti30m;
+    @Column(name = "ti40m")
+    Double ti40m;
+    @Column(name = "ti60m")
+    Double ti60m;
+    @Column(name = "ti80m")
+    Double ti80m;
+    @Column(name = "ti100m")
+    Double ti100m;
+
+    public Sample() {
+        direction30m = 0d;
+        direction40m = 0d;
+        direction60m = 0d;
+        direction80m = 0d;
+        direction100m = 0d;
+
+        speed30m = 0d;
+        speed40m = 0d;
+        speed60m = 0d;
+        speed80m = 0d;
+        speed100m = 0d;
+
+        ti30m = 0d;
+        ti40m = 0d;
+        ti60m = 0d;
+        ti80m = 0d;
+        ti100m = 0d;
+    }
 }
