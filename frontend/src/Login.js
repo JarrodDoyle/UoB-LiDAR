53a697479f1bbe2ed332b301f921ac89eb968bbf
danjones1618 <hello@danjones.dev>
2019-12-13 00:43:34 +0000

    Add random background images to Login page

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,9 +1,19 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import i1 from './res/login-bg1.jpg';
+import i2 from './res/login-bg2.jpg';
+import i3 from './res/login-bg3.jpg';
+import i4 from './res/login-bg4.jpg';
+import i5 from './res/login-bg5.jpg';
+import i6 from './res/login-bg6.jpg';
+import i7 from './res/login-bg7.jpg';
+import i8 from './res/login-bg8.jpg';
 
 export default function Login() {
+  let bgs = [i1,i2,i3,i4,i5,i6,i7,i8];
+  let bgnum = Math.floor(Math.random() * 8)
   return (
-    <div className="Login-container">
+    <div className="Login-container" style={{backgroundImage: `url(${bgs[bgnum]})`}}>
       <form className="Login">
         <h2>LiDAR</h2>
         <label for="email">Email:</label>
