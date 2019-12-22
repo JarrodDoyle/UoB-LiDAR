a07c5e010d802a9639daa9b4aed9880ba8241c41
zognotadog <alexasher@hotmail.co.uk>
2019-12-22 18:37:02 +0000

    Lidars Page Updates
    1. Rewrote to fit to other pages
    2. Added a shit map

--- frontend/src/Lidars.js
+++ frontend/src/Lidars.js
@@ -1,22 +1,133 @@
 import React from "react";
-import {
-  Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle, Button, Container
-} from 'reactstrap';
 import styles from './lidars.module.css';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps';
+import { Link } from "react-router-dom";
+import _ from "lodash";
+import GridLayout from 'react-grid-layout';
 
-function Lidar(props){
+function Card(props) {
   return (
-    <li>
-      <span>{props.name}</span>
-      <span>............</span>
-      <span>{props.loc}</span>
-    </li>
+    <>
+      <div className="lidars-card-content">
+        <h3>{props.title}</h3>
+        <div className="lidars-card-text">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae orci vel enim pretium scelerisque. Pellentesque eleifend nec odio imperdiet pretium. Mauris ullamcorper mi erat.</p> 
+            <div className="card-buttons">
+                <Link to="/Dashboard">
+                  <span className="text">Dashboard</span>
+                </Link>
+            </div>
+        </div>
+        <div className="lidars-card-map">
+            <WrappedMap
+            googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyAuy0y-1edccXfqufhhq3JFUa0NCBtUzsE"
+            loadingElement={<div style={{ height: `100%` }} />}
+            containerElement={<div style={{ height: `400px` }} />}
+            mapElement={<div style={{ height: `100%` }} />}
+            />
+        </div>
+      </div>    
+      
+    </>
   );
 }
 
-export default function Lidars() {
+function Map() {
+    return(
+        <GoogleMap 
+            defaultZoom={10}
+            defaultCenter={{lat:50.780519, lng: -0.152488}}
+        />
+    );
+}
+
+const WrappedMap = withScriptjs(withGoogleMap(Map))
+
+class CardGrid extends React.Component {
+    static defaultProps = {
+        className: "layout",
+        items: 3,
+        onLayoutChange: function() {},
+        cols: 1,
+        rows: 3,
+        rowHeight: 175,
+        width: 1830-80,
+        autoSize: true,
+        isDraggable: false,
+        isResizable: false,
+  };
+
+    constructor(props) {
+        super(props)
+        this.state = {
+          layout: this.generateLayout(),
+          cards: this.generateCards(),
+        }
+    }
+    
+    generateCards() {
+        return [{title: "LiDAR Site"},{title: "LiDAR Site"},{title: "LiDAR Site"}]
+    }
+
+    generateLayout() {
+    const p = this.props;
+    return _.map(new Array(p.items), function(item, i) {
+      return {
+        x: (i % p.cols),
+        y: Math.floor(i / p.cols),
+        w: 1,
+        h: 1,
+        i: i.toString()
+      };
+    });
+  }
+
+    generateDOM() {
+    var cards = this.state.cards
+    return _.map(_.range(this.props.items), function(i) {
+      return (
+        <div key={i}>
+          <Card 
+            title={cards[i].title}
+            content={"random"}
+            page="#" 
+            cardID={i}
+          />
+        </div>
+      );
+    });
+    }
+
+    onLayoutChange(layout) {
+    this.props.onLayoutChange(layout);
+  }
+
+    render() {
+        return (
+          <GridLayout 
+            className="layout" 
+            layout={this.state.layout}
+            onLayoutChange={this.onLayoutChange}
+            {...this.props} 
+          >
+            {this.generateDOM()}
+          </GridLayout>
+        )
+      }
+
+}
+
+function LiDARS(props) {
+    return (
+    <main>
+      <CardGrid/>
+    </main>
+  );
+}
+
+export default LiDARS
+
+function Lidars() {
     return (
       <main>
           <div className={styles.container}>
