9588089c1eaae21186faf4ce722390aa80c220c7
danjones1618 <hello@danjones.dev>
2020-01-23 17:30:01 +0000

    Add all login routes

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {Switch, Redirect, Route,Link} from "react-router-dom";
 import i1 from './res/login-bg1.jpg';
 import i2 from './res/login-bg2.jpg';
 import i3 from './res/login-bg3.jpg';
@@ -21,24 +21,89 @@ function MaterialInput(props){
   );
 }
 
+function LoginForm(){
+  return (
+    <form className="Login">
+      <h2>LiDAR</h2>
+      <MaterialInput type="email" name="email" label="Email"/>
+      <MaterialInput type="password" name="password" label="Password"/>
+      <div>
+        <h3>Login</h3>
+        <Link to="/Lidars" className="login-btn"><i className="fas fa-chevron-right"/></Link>
+      </div>
+      <div>
+        <Link to="/login/register">Create account</Link>
+        <Link to="/login/forgot">Forgot password</Link>
+      </div>
+    </form>
+  );
+}
+
+function RegistrationForm(){
+  return (
+    <form className="Login">
+    <h2>LiDAR</h2>
+      <MaterialInput type="email" name="email" label="Email"/>
+      <MaterialInput type="password" name="password" label="Password"/>
+      <MaterialInput type="password" name="password" label="Repeat Password"/>
+      <div>
+        <h3>Register</h3>
+        <Link to="/login" className="login-btn"><i className="fas fa-chevron-right"/></Link>
+      </div>
+      <div>
+        <Link to="/login/">Ive got a login</Link>
+      </div>
+    </form>
+  );
+}
+
+function ForgotForm(){
+   return (
+    <form className="Login">
+    <h2>Forgot pass</h2>
+      <MaterialInput type="email" name="email" label="Email"/>
+      <div>
+        <h3>Get reset link</h3>
+        <Link to="/login/forgot-sent" className="login-btn"><i className="fas fa-chevron-right"/></Link>
+      </div>
+      <div>
+        <Link to="/login/">Ive got a login</Link>
+      </div>
+    </form>
+  );
+}
+
+function ForgotSent(){
+  return (
+    <div className="Login">
+      <h2>Link sent</h2>
+      <p>If you entered a valid email address there will be a reset link in your inbox</p>
+    </div>
+  );
+}
+
 export default function Login() {
   let bgs = [i1,i2,i3,i4,i5,i6,i7,i8];
   let bgnum = Math.floor(Math.random() * 8)
   return (
     <div className="Login-container" style={{backgroundImage: `url(${bgs[bgnum]})`}}>
-      <form className="Login">
-        <h2>LiDAR</h2>
-        <MaterialInput type="email" name="email" label="Email"/>
-        <MaterialInput type="password" name="password" label="Password"/>
-        <div>
-          <h3>Login</h3>
-          <Link to="/Lidars" className="login-btn"><i className="fas fa-chevron-right"/></Link>
-        </div>
-        <div>
-          <Link to="#">Create account</Link>
-          <Link to="#">Forgot password</Link>
-        </div>
-      </form>
+      <Switch>
+        <Route exact path="/login">
+          <LoginForm/>
+        </Route>
+        <Route exact path="/login/register">
+          <RegistrationForm/>
+        </Route>
+        <Route exact path="/login/forgot">
+          <ForgotForm/>
+        </Route>
+        <Route exact path="/login/forgot-sent">
+          <ForgotSent/>
+        </Route>
+        <Route path="*">
+          <h1>noo</h1>
+        </Route>
+      </Switch>
     </div>
   );
 }
