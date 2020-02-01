fdc186e2a522dc924d1ee46d6f259106f150fd6f
danjones1618 <hello@danjones.dev>
2020-02-01 22:49:48 +0000

    Lighten circle button on hover

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -53,6 +53,7 @@ export default function App() {
       <Helmet>
         <title>LiDAR</title>
         <link rel="shortcut icon" href={icon}/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <script src="https://kit.fontawesome.com/b5b28e0ac4.js" crossorigin="anonymous"></script>
       </Helmet>
       <Switch>
