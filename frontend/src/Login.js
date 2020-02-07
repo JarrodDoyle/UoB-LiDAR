c8fbb718483703a8526d808edc9e8fd43b1a66d3
danjones1618 <hello@danjones.dev>
2020-02-07 13:56:49 +0000

    More bgs

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -32,6 +32,9 @@ import i28 from './res/login-bg28.jpg';
 import i29 from './res/login-bg29.jpg';
 import i30 from './res/login-bg30.jpg';
 import i31 from './res/login-bg31.jpg';
+import i32 from './res/login-bg32.jpg';
+import i33 from './res/login-bg33.jpg';
+import i34 from './res/login-bg34.jpg';
 import "./Login.css";
 
 class LoginForm extends React.Component{
@@ -155,7 +158,7 @@ export default function Login() {
   let bgs = [i1,i2,i3,i4,i5,i6,i7,i8,i9,
     i10,i11,i12,i13,i14,i15,i16,i17,i18,i19,
     i20,i21,i22,i23,i24,i25,i26,i27,i28,i29,
-    i30,i31];
+    i30,i31,i32,i33,i34];
   let bgnum = Math.floor(Math.random() * bgs.length)
   return (
     <div className="Login-container" style={{backgroundImage: `url(${bgs[bgnum]})`}}>
