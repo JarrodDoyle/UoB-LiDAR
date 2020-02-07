5ddc34207eae85b175104bec230d9a55ea6f2ec4
danjones1618 <hello@danjones.dev>
2020-02-07 16:14:29 +0000

    Add even more login backgrounds

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {Switch, Redirect, Route,Link} from "react-router-dom";
 import {MaterialInput} from './Material-Inp.js';
+import i0 from './res/login-bg0.jpg';
 import i1 from './res/login-bg1.jpg';
 import i2 from './res/login-bg2.jpg';
 import i3 from './res/login-bg3.jpg';
@@ -35,6 +36,31 @@ import i31 from './res/login-bg31.jpg';
 import i32 from './res/login-bg32.jpg';
 import i33 from './res/login-bg33.jpg';
 import i34 from './res/login-bg34.jpg';
+import i35 from './res/login-bg35.jpg';
+import i36 from './res/login-bg36.jpg';
+import i37 from './res/login-bg37.jpg';
+import i38 from './res/login-bg38.jpg';
+import i39 from './res/login-bg39.jpg';
+import i40 from './res/login-bg40.jpg';
+import i41 from './res/login-bg41.jpg';
+import i42 from './res/login-bg42.jpg';
+import i43 from './res/login-bg43.jpg';
+import i44 from './res/login-bg44.jpg';
+import i45 from './res/login-bg45.jpg';
+import i46 from './res/login-bg46.jpg';
+import i47 from './res/login-bg47.jpg';
+import i48 from './res/login-bg48.jpg';
+import i49 from './res/login-bg49.jpg';
+import i50 from './res/login-bg50.jpg';
+import i51 from './res/login-bg51.jpg';
+import i52 from './res/login-bg52.jpg';
+import i53 from './res/login-bg53.jpg';
+import i54 from './res/login-bg54.jpg';
+import i55 from './res/login-bg55.jpg';
+import i56 from './res/login-bg56.jpg';
+import i57 from './res/login-bg57.jpg';
+import i58 from './res/login-bg58.jpg';
+import i59 from './res/login-bg59.jpg';
 import "./Login.css";
 
 class LoginForm extends React.Component{
@@ -155,10 +181,13 @@ function ForgotSent(){
 }
 
 export default function Login() {
-  let bgs = [i1,i2,i3,i4,i5,i6,i7,i8,i9,
+  let bgs = [i0, i1,i2,i3,i4,i5,i6,i7,i8,i9,
     i10,i11,i12,i13,i14,i15,i16,i17,i18,i19,
     i20,i21,i22,i23,i24,i25,i26,i27,i28,i29,
-    i30,i31,i32,i33,i34];
+    i30,i31,i32,i33,i34,i35,i36,i37,i38,i39,
+    i40,i41,i42,i43,i44,i45,i46,i47,i48,i49,
+    i50,i51,i52,i53,i54,i55,i56,i57,i58,i59
+  ];
   let bgnum = Math.floor(Math.random() * bgs.length)
   return (
     <div className="Login-container" style={{backgroundImage: `url(${bgs[bgnum]})`}}>
