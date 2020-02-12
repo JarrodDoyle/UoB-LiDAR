50f2856ba74f698ed76c1bd00c991953417bec71
danjones1618 <hello@danjones.dev>
2020-02-12 11:44:07 +0000

    Revert "Fix bottom radius of lidar maps"
    This reverts commit 90b244b8a0ff08c8c9cc53e7dc43d547a1905253.

--- frontend/src/Lidars.js
+++ frontend/src/Lidars.js
@@ -10,9 +10,9 @@ function Card(props) {
       <div className="lidars-card-map">
         <WrappedMap
           googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyAuy0y-1edccXfqufhhq3JFUa0NCBtUzsE"
-          loadingElement={<div style={{ height: `100%` }} />}
-          containerElement={<div style={{ height: `100%` }} />}
-          mapElement={<div style={{ height: `100%` }} />}
+          loadingElement={<div style={{ height: `100%`, borderRadius: '10px' }} />}
+          containerElement={<div style={{ height: `100%`, borderRadius: '10px' }} />}
+          mapElement={<div style={{ height: `100%`, borderRadius: '10px' }} />}
           location={props.location}
         />
       </div>
