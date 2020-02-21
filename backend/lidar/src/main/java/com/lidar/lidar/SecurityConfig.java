407a6cab350694d229e6d457a4bee87cfaef8295
Jarrod Doyle <ff18713@bristol.ac.uk>
2020-02-21 12:06:16 +0000

    Revert "Merge branch 'dev' into fontend"
    This reverts commit b66bb3733550d3642989445f02bd98ee26d9d6ad.

--- backend/lidar/src/main/java/com/lidar/lidar/SecurityConfig.java
+++ backend/lidar/src/main/java/com/lidar/lidar/SecurityConfig.java
@@ -1,21 +1,17 @@
 package com.lidar.lidar;
 
-import org.springframework.context.annotation.*;
-import org.springframework.security.config.annotation.web.configuration.*;
-import org.springframework.security.config.annotation.web.builders.*;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.authorizeRequests()
-				.antMatchers("/test/**").hasIpAddress("localhost").and()
-			.authorizeRequests()
-				.regexMatchers("^/(?!test).*$").authenticated().and()
-			.regexMatcher("^/(?!test).*$").requiresChannel()
-				.anyRequest().requiresSecure();
-
-	}
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .requiresChannel()
+            .anyRequest()
+            .requiresSecure();
+    }
 }
+
--- backend/lidar/src/main/java/com/lidar/lidar/database/SpeedHeightTable.java
+++
@@ -1,7 +0,0 @@
-package com.lidar.lidar.database;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface SpeedHeightTable extends CrudRepository<SpeedHeight, String> {
-
-}
