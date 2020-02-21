75d0dad69fec7b1add64beb6c42b0bac23d3a375
TBExtent <jedpreist@gmail.com>
2020-02-21 15:55:55 +0000

    Merge branch 'dev' into springBootSetup

--- frontend/src/Dashboard.js
+++ frontend/src/Dashboard.js
@@ -10,56 +10,121 @@ const mydata = [
     "color": "hsl(227, 70%, 50%)",
     "data": [
       {
-        "x": "plane",
+        "x": 0,
         "y": 256
       },
       {
-        "x": "helicopter",
+        "x": 1,
         "y": 88
       },
       {
-        "x": "boat",
+        "x": 2,
         "y": 2
       },
       {
-        "x": "train",
+        "x": 3,
         "y": 170
       },
-      {
-        "x": "subway",
-        "y": 79
-      },
-      {
-        "x": "bus",
-        "y": 246
-      },
-      {
-        "x": "car",
-        "y": 126
-      },
-      {
-        "x": "moto",
-        "y": 169
-      },
-      {
-        "x": "bicycle",
-        "y": 3
-      },
-      {
-        "x": "horse",
-        "y": 284
-      },
-      {
-        "x": "skateboard",
-        "y": 50
-      },
-      {
-        "x": "others",
-        "y": 41
-      }
     ]
   }
 ]
+
+function Popup(props) {
+  let names = ["fas fa-check ok", "fas fa-bacon almost", "fas fa-times bad"]
+  let style = names[Math.floor(Math.random() * 3)];
+  let kpiTitles = [
+    "System Availability", 
+    "Post Processed Data Availability",
+    "Data Coverage",
+    "Maintenance Visits",
+    "Unscheduled Outages",
+    "Comms. Uptime",
+    "Mean Wind Speed",
+    "Mean Wind Direction",
+    "Turbulence Intensity",
+    "Wind Shear",
+  ]
+
+  for( var i = 0; i < kpiTitles.length; i++){ 
+    if ( kpiTitles[i] === props.currentKPI) {
+      kpiTitles.splice(i, 1); 
+    }
+  }
+  kpiTitles.unshift(props.currentKPI)
+
+  return (
+    <div className="popup-background">
+      <div className="popup-box">
+        <div className="popup-header">
+          <select className="kpi-dropdown">
+            {kpiTitles.map((kpi, i) => {       
+              return (<option value={i}>{kpi}</option>) 
+            })}
+          </select>
+          <div className="kpi-timescale">
+            <span>Week</span>
+            <span>Month</span>
+            <span>6 Months</span>
+            <span>Year</span>
+          </div>
+          <i className={style}></i>
+          <div className="lidars-btns">
+            <button className="circle-btn" onClick={() => props.togglePopup(null)}>
+              <i className="fas fa-times"/>
+            </button>
+          </div>
+        </div>
+        <div className="popup-grid">
+          <div className="popup-grid-item-tall">
+            <Graph data={mydata}/>
+          </div>
+          <div className="popup-grid-item">
+            <Graph data={mydata}/>
+          </div>
+          <div className="popup-grid-item">
+            <Graph data={mydata}/>
+          </div>
+          <div className="popup-grid-item-fat">
+            <Graph data={mydata}/>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function Graph(props) {
+  let commonProps = {
+    margin: { top: 20, right: 20, bottom: 40, left: 40 },
+    animate: true,
+    enableSlices: 'x',
+  }
+  return(
+    <AutoSizer>
+      {({ height, width }) => (
+        // <div className="lidars-card" style={`width: ${width};`}>
+          <Line
+            {... commonProps}
+            data={props.data}
+            height={height}
+            width={width}
+            xScale={{
+              type: 'point',
+              min: 0,
+              max: 'auto',
+            }}
+            axisLeft={{
+              legend: 'y-axis',
+            }}
+            axisBottom={{
+              legend: 'x-axis',
+            }}
+          />
+        // </div>
+      )}
+    </AutoSizer>        
+  )
+}
 
 function Card(props) {
   let names = ["fas fa-check ok", "fas fa-bacon almost", "fas fa-times bad"]
@@ -77,42 +142,60 @@ function Card(props) {
       </div>
       <div className="lidars-btns">
         <h4>More details</h4>
-        <Link className="circle-btn" to="/Dashboard">
+        <button className="circle-btn" onClick={() => props.togglePopup(props.title)}>
           <i className="fas fa-chevron-right"/>
-        </Link>
+        </button>
       </div>
     </div>
   );
 }
 
-function Dashboard(props) {
-  return (
-    <main>
-      <div className="lidars-grid">
-        <Card title="System Availability" content={["1 month average - 91%", "Campaign average - 97%"]}/>
-        <Card title="Post Processed Data Availability" content={["1 month average - 88%", "Campaign average - 88%"]}/>
-        <Card title="Data Coverage" content={["something"]}/>
-        <Card title="Maintenance Visits" content={["0"]}/>
-        <Card title="Unscheduled Outages" content={["0"]}/>
-        <Card title="Comms. Uptime" content={["100%"]}/>
-        <Card title="Mean Wind Speed" content={["Slope - 1.00", "Coefficient of Determination - 1.00"]}/>
-        <Card title="Mean Wind Direction" content={["Slope - 1.00", "Coefficient of Determination - 1.00"]}/>
-        <Card title="Turbulence Intensity" content={["Slope - x", "Correlation Co-efficient - x"]}/>
-        <Card title="Wind Shear" content={["Shear exponent - x"]}/>
-        <div className="lidars-card">
-          <AutoSizer>
-            {({ height, width }) => (
-              <Line
-                data={mydata}
-                height={height}
-                width={width}
-              />
-            )}
-          </AutoSizer>        
+class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showPopup: false,
+      popupTitle: null,
+    };
+    this.togglePopup = this.togglePopup.bind(this);
+    this.openPopup = this.openPopup.bind(this);
+  }
+
+  togglePopup() {
+    this.setState({
+      showPopup: !this.state.showPopup,
+    });
+  }
+
+  openPopup(newPopupTitle) {
+    this.togglePopup();
+    this.setState( {
+      popupTitle: newPopupTitle,
+    })
+  }
+
+  render() {
+    return (
+      <main>
+        <div className="lidars-grid">
+          <Card title="System Availability" content={["1 month average - 91%", "Campaign average - 97%"]} togglePopup={this.openPopup}/>
+          <Card title="Post Processed Data Availability" content={["1 month average - 88%", "Campaign average - 88%"]} togglePopup={this.openPopup}/>
+          <Card title="Data Coverage" content={["something"]} togglePopup={this.openPopup}/>
+          <Card title="Maintenance Visits" content={["0"]} togglePopup={this.openPopup}/>
+          <Card title="Unscheduled Outages" content={["0"]} togglePopup={this.openPopup}/>
+          <Card title="Comms. Uptime" content={["100%"]} togglePopup={this.openPopup}/>
+          <Card title="Mean Wind Speed" content={["Slope - 1.00", "Coefficient of Determination - 1.00"]} togglePopup={this.openPopup}/>
+          <Card title="Mean Wind Direction" content={["Slope - 1.00", "Coefficient of Determination - 1.00"]} togglePopup={this.openPopup}/>
+          <Card title="Turbulence Intensity" content={["Slope - x", "Correlation Co-efficient - x"]} togglePopup={this.openPopup}/>
+          <Card title="Wind Shear" content={["Shear exponent - x"]} togglePopup={this.openPopup}/>
         </div>
-      </div>
-    </main>
-  );
+        {this.state.showPopup ?  
+          <Popup togglePopup={this.togglePopup} currentKPI={this.state.popupTitle}/>  
+          : null  
+        }  
+      </main>
+    );
+  }
 }
 
 export default Dashboard
--- frontend/src/Settings.js
+++ frontend/src/Settings.js
@@ -1,8 +1,90 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from 'react-redux';
+import { findByLabelText } from "@testing-library/react";
+import {MaterialInput} from "./Material-Inp.js";
 
-export default function Settings() {
- return (
-   <main>
+
+
+class CredsCard extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {redirect: false};
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  async handleSubmit(event){
+    //Handle Submit
+  }
+
+  render() {
+    return (
+      <div className="settings-card">
+        <form className="material-form" onSubmit={this.handleSubmit}>
+          <h1>User Credentials</h1>
+          <MaterialInput type="email" name="email" label="Email"/>  
+          <MaterialInput type="password" name="password" label="Password"/>
+          <div className="elipticle-btn" style={{width: 170}}>
+          <h5>Submit <i className="fas fa-chevron-right"/></h5>
+          </div>
+        </form>
+      </div>
+    );
+   }
+}
+
+class ApiCard extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {redirect: false};
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  async handleSubmit(event){
+    //Handle Submit
+  }
+  render() {
+    return (
+      <div className="settings-card">
+        <form className="material-form" onSubmit={this.handleSubmit}>
+          <h1>Api Credentials</h1>  
+          <MaterialInput type="text" name="apikey" label="Google Maps API Key"/>
+          <div className="elipticle-btn" style={{width: 170}}>
+          <h5>Submit <i className="fas fa-chevron-right"/></h5>
+          </div>
+        </form>
+
+      </div>
+    );
+   }
+}
+
+function AdminSettingsCard(props) {
+  return (
+    <div className="settings-card">
+       <h1>Admin Settings</h1>
+       <h3>Assign lidar to account</h3>
+        <h3>Change user perms</h3>
+        <h3>Edit lidar (need to add edit link to Lidars page) - with add lidar link</h3>
+    </div>
+  )
+}
+
+
+function Settings(props) {
+  return (
+  <main className="lidars-wrapper">
+    <div className="settings-grid">
+      <CredsCard></CredsCard>
+      <ApiCard></ApiCard>
+      <AdminSettingsCard></AdminSettingsCard>
+      </div>
+  </main>
+  );
+}
+export default connect(state => ({sites: state.sites}))(Settings)
+
+{/* <main>
     <section>
       <h2>Credentials</h2>
     </section>
@@ -15,6 +97,4 @@ export default function Settings() {
       <h3>Change user perms</h3>
       <h3>Edit lidar (need to add edit link to Lidars page) - with add lidar link</h3>
     </section>
-   </main>
- );
-}
+   </main> */}
