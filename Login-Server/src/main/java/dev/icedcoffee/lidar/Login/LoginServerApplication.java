300ea83fe5cb471eb2287ebf416cff072ded5091
danjones1618 <hello@danjones.dev>
2020-02-21 15:34:32 +0000

    Add test login route

--- Login-Server/src/main/java/dev/icedcoffee/lidar/Login/LoginServerApplication.java
+++ Login-Server/src/main/java/dev/icedcoffee/lidar/Login/LoginServerApplication.java
@@ -4,10 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class LoginServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LoginServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LoginServerApplication.class, args);
+    }
 
+    @PostMapping("/login")
+    public String testCreate(@RequestParam(name="username", required=true) String username,
+            @RequestParam(name="password", required=true) String password) {
+        return "Login route"
+    }
 }
