f39f35fea89d9268ebd4b6b3b031d77c8d165310
danjones1618 <hello@danjones.dev>
2020-02-21 15:21:46 +0000

    🐣 Initial Login Server Commit

---
+++ Login-Server/src/test/java/dev/icedcoffee/lidar/Login/LoginServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.icedcoffee.lidar.Login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
