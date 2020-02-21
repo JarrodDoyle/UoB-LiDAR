25495140f5f1f58f11b5be5a34d822cc94102729
TBExtent <jedpreist@gmail.com>
2020-02-21 16:06:44 +0000

    add more spaces

--- backend/lidar/src/main/java/com/lidar/lidar/database/BuoyTable.java
+++ backend/lidar/src/main/java/com/lidar/lidar/database/BuoyTable.java
@@ -2,7 +2,7 @@ package com.lidar.lidar.database;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface BuoyTable extends CrudRepository<Buoy, String> {
+public interface BuoyTable extends CrudRepository<Buoy, String> {   
 
 }
    
