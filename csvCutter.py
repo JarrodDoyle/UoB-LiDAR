4b34bc28a1249e878c562719570a28b8c9504343
TBExtent <jedpreist@gmail.com>
2020-03-02 13:45:17 +0000

    Readd data after the git-icloud incident

--- csvCutter.py
+++ csvCutter.py
@@ -12,11 +12,12 @@ for name in fileNames:
     
     for line in lines[3:]:
         if date != line[:10]:
-            csvOut = open(name[2:-4] + " part " + str(part) + ".csv", "w")
+            date = line[:10]
+
+            csvOut = open(name[2:-4] + "_" + date + ".csv", "w")
             for header in headers:
                 csvOut.write(header)
 
-            date = line[:10]
             part += 1
 
         csvOut.write(line)
