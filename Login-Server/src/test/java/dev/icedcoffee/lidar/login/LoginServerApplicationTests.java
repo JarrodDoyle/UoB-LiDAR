1326c0de11e13922b471c8413bfd1f25c6240ad1
danjones1618 <hello@danjones.dev>
2020-03-06 15:50:10 +0000

    Write a test

--- Login-Server/src/test/java/dev/icedcoffee/lidar/login/LoginServerApplicationTests.java
+++ Login-Server/src/test/java/dev/icedcoffee/lidar/login/LoginServerApplicationTests.java
@@ -3,6 +3,23 @@ package dev.icedcoffee.lidar.login;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.matchesPattern;   
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.*;
+import org.springframework.test.web.servlet.request.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 @SpringBootTest
 class LoginServerApplicationTests {
 
@@ -15,7 +32,10 @@ class LoginServerApplicationTests {
 
 	@Test
 	void registration() {
-
+		mvc.perform(MockMvcRequestBuilders.post("/register?email=a@a.com&password=a")
+			.with(csrf())
+			.accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
 	}
 
 }
