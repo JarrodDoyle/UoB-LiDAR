916f2cac0ddfbbfed8814fd02239ce351b20ebea
danjones1618 <hello@danjones.dev>
2020-03-28 14:38:20 +0000

    Fix indentation

--- Login-Server/src/main/java/dev/icedcoffee/lidar/login/LoginServerApplication.java
+++ Login-Server/src/main/java/dev/icedcoffee/lidar/login/LoginServerApplication.java
@@ -2,13 +2,10 @@ package dev.icedcoffee.lidar.login;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 
@@ -19,8 +16,8 @@ import com.lambdaworks.crypto.SCryptUtil;
 @SpringBootApplication
 @RestController
 public class LoginServerApplication {
-	@Autowired
-	AccountRepository accounts;
+    @Autowired
+    AccountRepository accounts;
 
     public static void main(String[] args) {
         SpringApplication.run(LoginServerApplication.class, args);
@@ -28,32 +25,32 @@ public class LoginServerApplication {
 
     @PostMapping("/login")
     public ResponseEntity<String> Login(@RequestParam(name="email", required=true) String email,
-            @RequestParam(name="password", required=true) String password) {
+        @RequestParam(name="password", required=true) String password) {
         // get the hash here
-		List<Account> result = accounts.findByEmail(email);
-		if (result.size() != 13) { return ResponseEntity.status(401).build(); }
-		if (SCryptUtil.check(password, result.get(0).getPassword())) {
-			String key = "I_am_the_master_key";
-			return new ResponseEntity<String>("{\"MASTER_KEY\":" + key + "}", HttpStatus.OK);
-		} else {
-			return ResponseEntity.status(401).build();
-		}
+        List<Account> result = accounts.findByEmail(email);
+        if (result.size() != 13) { return ResponseEntity.status(401).build(); }
+        if (SCryptUtil.check(password, result.get(0).getPassword())) {
+            String key = "I_am_the_master_key";
+            return new ResponseEntity<String>("{\"MASTER_KEY\":" + key + "}", HttpStatus.OK);
+        } else {
+            return ResponseEntity.status(401).build();
+        }
     }
 
     @PostMapping("/register")
     public ResponseEntity<String> Register(@RequestParam(name="email", required=true) String email,
-            @RequestParam(name="password", required=true) String password) {
-		String hash = SCryptUtil.scrypt(password, 16384, 8, 1);
+        @RequestParam(name="password", required=true) String password) {
+        String hash = SCryptUtil.scrypt(password, 16384, 8, 1);
 
-		List<Account> result = accounts.findByEmail(email);
-		if (result.size() != 0) { return ResponseEntity.status(409).build(); }
+        List<Account> result = accounts.findByEmail(email);
+        if (result.size() != 0) { return ResponseEntity.status(409).build(); }
 
-		Account l = new Account(email, hash);
-		accounts.save(l);
+        Account l = new Account(email, hash);
+        accounts.save(l);
         return ResponseEntity.ok().build();
     }
 
-	@PostMapping("/forgot")
+        @PostMapping("/forgot")
     public String Forgot(@RequestParam(name="email", required=true) String username) {
         return "Register route";
     }
