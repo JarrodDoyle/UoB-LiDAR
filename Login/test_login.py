1439cc4c3fa3600b84fc8b49915e8ef221a41ace
danjones1618 <hello@danjones.dev>
2020-05-17 02:11:52 +0100

    Account test

--- Login/test_login.py
+++ Login/test_login.py
@@ -12,8 +12,26 @@ def login(client, email, password):
     return client.post('/login', json={
         "email": email,
         "password": password
-        }, follow_redirects=True)
+    }, follow_redirects=True)
+
+def register(client, email, password):
+    return client.post('/register', json={
+        "email": email,
+        "password": password
+    }, follow_redirects=True)
+
+def test_register(client):
+    rv = register(client, "a@a.com", "a")
+    assert b'Success' in rv.data
+
+def test_register_exists(client):
+    rv = register(client, "a@a.com", "a")
+    assert b'Account already exists' in rv.data
 
 def test_login(client):
     rv = login(client, "a@a.com", "a")
     assert b'master_key' in rv.data
+
+def test_login_bad(client):
+    rv = login(client, "b@b.com", "b")
+    assert b'Invalid username or password' in rv.data
